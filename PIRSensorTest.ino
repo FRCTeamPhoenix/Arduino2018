3013ff3314cdf19773725eae5c632c29bc627b44
Akhi314 <31868879+Akhi314@users.noreply.github.com>
2018-02-01 20:05:57 -0500

    Add files via upload

---
+++ PIRSensorTest.ino
@@ -0,0 +1,25 @@
+int val = 0;
+int pirState = LOW;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(7, INPUT);
+pinMode(4, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+val = digitalRead(7);
+if (val == HIGH) {
+  digitalWrite(4, HIGH);
+  if (pirState == LOW) {
+    pirState = HIGH;
+  }
+ 
+} else {
+  digitalWrite(4, LOW);
+  if (pirState == HIGH) {
+    pirState = LOW;
+  }
+}
+}
