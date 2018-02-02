3b9526fdd1a5e69c14cf2440bac70ff19d30df73
Akhi314 <31868879+Akhi314@users.noreply.github.com>
2018-02-01 20:57:36 -0500

    Add files via upload

---
+++ Photocell.ino
@@ -0,0 +1,22 @@
+int photocellPin = 0;     
+int photocellReading = 0;     
+int LEDpin = 11;          
+int LEDbrightness;       
+void setup(void) {
+  Serial.begin(9600);   
+}
+ 
+void loop(void) {
+  photocellReading = analogRead(photocellPin);  
+ 
+  Serial.print("Analog reading = ");
+  Serial.println(photocellReading);     
+ 
+  
+  photocellReading = 1023 - photocellReading;
+ 
+  LEDbrightness = map(photocellReading, 0, 1023, 0, 255);
+  analogWrite(LEDpin, LEDbrightness);
+ 
+  delay(100);
+}
